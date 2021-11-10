69d8cada10cc521bd8ebaf54bddfed357dc6cdc5
PtitNost <corentin.freire@gmail.com>
2021-11-10 22:39:36 +0100

    Refactor + FastAPI Added

--- client/static/js/devices.js
+++ client/static/js/devices.js
@@ -5,9 +5,9 @@ ubiquiti.addEventListener('click', (e) => {
     ubiquiti.classList.toggle('active')
     let ubiquiti_content = document.querySelector("#ubiquiti_content")
     if (ubiquiti_content.style.display === "flex") {
-      ubiquiti_content.style.display = "none";
+        ubiquiti_content.style.display = "none";
     } else {
-      ubiquiti_content.style.display = "flex";
+        ubiquiti_content.style.display = "flex";
     }
 })
 
@@ -15,8 +15,8 @@ dasan.addEventListener('click', (e) => {
     dasan.classList.toggle('active')
     let dasan_content = document.querySelector("#dasan_content")
     if (dasan_content.style.display === "flex") {
-      dasan_content.style.display = "none";
+        dasan_content.style.display = "none";
     } else {
-      dasan_content.style.display = "flex";
+        dasan_content.style.display = "flex";
     }
 })
