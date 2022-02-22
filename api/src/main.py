a2b2f584f7a400089e8e46b9a0b5de0a6a76754e
Alexis LEBEL <lebalexis@gmail.com>
2022-02-22 21:22:09 +0100

    Changed execution order

--- api/src/main.py
+++ api/src/main.py
@@ -43,8 +43,8 @@ app.add_middleware(
 
 if __name__ == "__main__":
     server.run_forever(threaded=True)
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
     threading.Thread(target=listener).start()
     print('listener on')
     run_cache()
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
