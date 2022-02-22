45e27e3b5f94b5e00b8dea51731ae311d5b99257
Alexis LEBEL <lebalexis@gmail.com>
2022-02-22 21:05:15 +0100

    Changed port primitive type

--- api/src/main.py
+++ api/src/main.py
@@ -42,7 +42,7 @@ app.add_middleware(
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port="8000")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
     server.run_forever(threaded=True)
 
     threading.Thread(target=listener).start()
