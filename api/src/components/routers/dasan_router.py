40a8a51596bc092d54c9bea42e8aea2e530123c6
PtitNost <corentin.freire@gmail.com>
2022-02-24 14:49:31 +0100

    Api post description onu

--- api/src/components/routers/dasan_router.py
+++ api/src/components/routers/dasan_router.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter, Depends
-
+from pydantic import BaseModel
 from ..dependencies import get_current_user
 from ..redis.redis_client import rclient
 from ..handlers.dasan_handler import DasanWorkflow
+
+class Onudesc(BaseModel):
+    onuid: int
+    desc: str
 
 router = APIRouter(prefix="/api/v1.0/ressources/dasan",
                    tags=["dasan"],
@@ -21,7 +25,7 @@ async def onus():
     return rclient.json().get('onus-dasan')
 
 @router.post("/desconu")
-async def nameonu(onuid=int, desc= str):
+async def nameonu(onudesc: Onudesc):
     dasan_workflow = DasanWorkflow('10.59.10.20')
-    dasan_workflow.set_onu_desc(onuid, desc)
+    dasan_workflow.set_onu_desc(onudesc.onuid, onudesc.desc)
     return "Nom changé"
