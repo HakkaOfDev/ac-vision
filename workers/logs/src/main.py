aa4c9d4b5518e357b9d4b126a3b4b53c707f2f6d
PtitNost <corentin.freire@gmail.com>
2022-02-25 15:28:14 +0100

    Test with Workers Websockets

--- workers/logs/src/main.py
+++ workers/logs/src/main.py
@@ -4,8 +4,10 @@ import re
 import json
 import threading
 from websocket_server import WebsocketServer
+import websockets
+import asyncio
 
-
+'''
 def register_new_client(client, server):
     print(f"New client added ! {client['address'][0]} on port {client['address'][1]}")
 
@@ -15,13 +17,14 @@ def on_message_received(client,server,message):
 server = WebsocketServer(host='0.0.0.0', port=6969, loglevel=logging.INFO)
 server.set_fn_new_client(register_new_client)
 server.set_fn_message_received(on_message_received)
-
+'''
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', 514))
 
+
 REGEX = r"(?P<onu>ONU\([0-9],[0-9]*\)) (?P<status>(DE)?ACTIVATION) \(Reason: (?P<reason>[\w\s\(\)]*)\)"
 
-def listen():
+async def listen(websocket, path):
     while True:
         print('listening...')
         data = s.recv(4048)
@@ -33,8 +36,16 @@ def listen():
                         "status": matches.group("status"),
                         "reason": matches.group("reason")}
             print(onu_info)
-            server.send_message_to_all(json.dumps(onu_info))
+            await websocket.send(json.dumps(onu_info))
+            #server.send_message_to_all(json.dumps(onu_info))
+            
+            
+start_server = websockets.serve(listen, "0.0.0.0", 6969)
+
+
 
 if __name__ == '__main__':
-    server.run_forever(threaded=True)
-    listen()
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+    #server.run_forever(threaded=True)
+    #listen()
