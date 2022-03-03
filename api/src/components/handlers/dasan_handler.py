33849685ecbd460f83c4d287c63a0ef4d001e7e5
PtitNost <corentin.freire@gmail.com>
2022-03-03 16:07:49 +0100

    test get onu description

--- api/src/components/handlers/dasan_handler.py
+++ api/src/components/handlers/dasan_handler.py
@@ -3,6 +3,7 @@ from ..tools.snmp.oids import OIDS
 from ..tools.snmp.snmp_utils import SnmpUtils
 from ..tools.time_utils import format_uptime, format_dasan_olt_uptime
 from ..sql_app import fonction
+from sqlalchemy.orm import Session
 
 class DasanWorkflow:
 
@@ -24,6 +25,7 @@ class DasanWorkflow:
         }
 
     def get_onus(self):
+        db: Session = Depends(fonction.get_db)
         olt = SnmpUtils(self.ip)
         onus_table = olt.get_table(OIDS.ONU_TABLE.value)
         onus_table.pop(-1)
@@ -40,7 +42,7 @@ class DasanWorkflow:
                     "macAddress": onu.get('sleGponOnuHwAddress').replace(' ', ':')[:-1],
                     "distance": (onu.get('sleGponOnuDistance').split(' ')[0], '')[onu.get('sleGponOnuRxPower') is None],
                     "profile": onu.get('sleGponOnuProfile'),
-                    "displayName": fonction.get_onusd(db = Depends(fonction.get_db),onuid=int(onu.get('sleGponOnuId'))),
+                    "displayName": fonction.get_onusd(db, onuid=int(onu.get('sleGponOnuId'))),
                     "serialNumber": onu.get('sleGponOnuSerial'),
                     "status": onu.get('sleGponOnuStatus'),
                     "uptime": (format_uptime(int(onu.get('sleGponOnuLinkUpTime').split(' ')[0]), 4), '')[
