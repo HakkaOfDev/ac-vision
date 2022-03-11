3eb4b2312e71c138df23179e00dbc94a3aef676d
HakkaOfDev <frestytv@gmail.com>
2022-03-11 14:39:08 +0100

    Test logger

--- workers/logs/src/main.py
+++ workers/logs/src/main.py
@@ -1,17 +1,13 @@
 import socket
 import re
-import json
-import socketio
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', 514))
 
 if __name__ == '__main__':
-    sio = socketio.Client()
-    sio.connect('http://ac-vision/ws')
     REGEX = r"(?P<onu>ONU\([0-9],[0-9]*\)) (?P<status>(DE)?ACTIVATION) \(Reason: (?P<reason>[\w\s\(\)]*)\)"
     while True:
-        data = s.recv(4048)
+        data = s.recv(4096)
         data = data.decode('utf-8')
         print(data)
         matches = re.search(REGEX, data)
@@ -20,6 +16,5 @@ if __name__ == '__main__':
                         "status": matches.group("status"),
                         "reason": matches.group("reason")}
             print(onu_info)
-            sio.emit('ONU', json.dumps(onu_info))
         else:
             print('No matches found')
