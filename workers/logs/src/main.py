98c76ae118915aff0245a404e998fe0076ffd372
HakkaOfDev <frestytv@gmail.com>
2022-03-11 14:47:07 +0100

    [LOGGER] New buffsize

--- workers/logs/src/main.py
+++ workers/logs/src/main.py
@@ -5,9 +5,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', 514))
 
 if __name__ == '__main__':
+    print('Start')
     REGEX = r"(?P<onu>ONU\([0-9],[0-9]*\)) (?P<status>(DE)?ACTIVATION) \(Reason: (?P<reason>[\w\s\(\)]*)\)"
     while True:
-        data = s.recv(4096)
+        data = s.recv(4048)
         data = data.decode('utf-8')
         print(data)
         matches = re.search(REGEX, data)
