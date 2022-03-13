9b8295b40debc520921483963fd37bc0635b9be8
PtitNost <corentin.freire@gmail.com>
2022-03-13 16:46:53 +0100

    [API] Patch ip olt

--- api/src/components/redis/cache_updates.py
+++ api/src/components/redis/cache_updates.py
@@ -13,13 +13,12 @@ from ..sql_app.fonction import get_oltip
 
 tl = Timeloop()
 
-#session = SessionLocal()
-#ip = get_oltip(session).ip
+
 
 @tl.job(interval=timedelta(seconds=15))
 def update_cache():
-
-    dasan_workflow = DasanWorkflow("10.59.20.10")
+    session = SessionLocal()
+    dasan_workflow = DasanWorkflow(get_oltip(session).ip)
     rclient.json().set('olt-dasan', Path.rootPath(), dasan_workflow.get_olt())
     rclient.json().set('onus-dasan', Path.rootPath(), dasan_workflow.get_onus())
     rclient.json().set('onus-activity', Path.rootPath(), dasan_workflow.get_onus_active())
@@ -32,7 +31,8 @@ def update_cache():
 
 @tl.job(interval=timedelta(seconds=1))
 def update_cache():
-    dasan_workflow = DasanWorkflow("10.59.20.10")
+    session = SessionLocal()
+    dasan_workflow = DasanWorkflow(get_oltip(session).ip)
 
 def run_cache():
     tl.start(block=False)
