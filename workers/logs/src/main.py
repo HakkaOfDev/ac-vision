787f0e276ad23ff24a9d51fc0052e890fc990dc7
HakkaOfDev <frestytv@gmail.com>
2022-03-15 11:29:40 +0100

    [LOGGER] socketserver args

--- workers/logs/src/main.py
+++ workers/logs/src/main.py
@@ -12,7 +12,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', 514))
 REGEX = r"(?P<onu>ONU\([0-9],[0-9]*\)) (?P<status>(DE)?ACTIVATION) \((Reason: )?(?P<reason>[\w\s\(\)]*)\)"
 
-def listen():
+def listen(serverSocket):
     while True:
         data,addr = s.recvfrom(4048)
         data = data.decode('utf-8')
@@ -22,13 +22,13 @@ def listen():
             onu_info = {"onu": matches.group("onu"),
                         "status": matches.group("status"),
                         "reason": matches.group("reason")}
-            sio.emit('ONU', onu_info)
+            serverSocket.emit('ONU', onu_info)
 
 @sio.on('READY')
 def ready(sid, data):
     print('Ready to listening..')
     print(data)
-    asyncio.create_task(listen())
+    asyncio.create_task(listen(sio))
 
 @sio.event
 def connect(sid, environ, auth):
