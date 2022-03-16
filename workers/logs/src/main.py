e295cc8d0f735fadb0fde9bb352074a9e1971176
HakkaOfDev <frestytv@gmail.com>
2022-03-16 17:40:27 +0100

    [LOGGER] Date notification and onuid

--- workers/logs/src/main.py
+++ workers/logs/src/main.py
@@ -1,13 +1,15 @@
 import socket
 import re
 import socketio
+from datetime import datetime
+import time
 
 sio = socketio.Client()
 sio.connect('http://server:6969')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', 514))
-REGEX = r"(?P<onu>ONU\([0-9],[0-9]*\)) (?P<status>(DE)?ACTIVATION) \((Reason: )?(?P<reason>[\w\s\(\)]*)\)"
+REGEX = r"(?P<onu>ONU\([0-9],[0-9]*\)) (?P<status>(DE)?ACTIVATION) (\(Reason: (?P<reason>[\w\s\(\)]*)\))?"
 
 if __name__ == '__main__':
     while True:
@@ -16,8 +18,9 @@ if __name__ == '__main__':
         matches = re.search(REGEX, data)
         print(data)
         if matches:
-            onu_info = {"onu": matches.group("onu"),
+            onu_info = {"onuid": matches.group("onu").split(',')[:1],
                         "status": matches.group("status"),
-                        "reason": matches.group("reason")}
+                        "reason": matches.group("reason"),
+                        "date": time.sstrftime("%a, %d %b %Y %H:%M:%S", datetime.now())}
             print(onu_info)
             sio.emit('ONU_INFO', onu_info)
