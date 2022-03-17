7743daa2f3f15072619809461f1e46517c1acdc4
HakkaOfDev <frestytv@gmail.com>
2022-03-17 12:07:35 +0100

    [SERVER] New url

--- workers/server/src/main.py
+++ workers/server/src/main.py
@@ -11,7 +11,7 @@ def connect(sid, environ, auth):
 
 @sio.on('ONU_INFO')
 def onu_balancer(sid, data):
-    requests.post('http://ac-vision/api/v1.0/ressources/notification/new', data=data, headers={'Accept':'application/json'})
+    requests.post('http://api:8000/api/v1.0/ressources/notification/new', data=data, headers={'Accept':'application/json'})
     sio.emit('ONU', data)
 
 @sio.event
